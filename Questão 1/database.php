dd21cbcd15984e7f01346773fb3a6ebc9051c3d5
Ana Beatriz Barreto Ribeiro <bibicachucle82@gmail.com>
2024-11-14 13:58:44 -0300

    database.php

--- Questão 1/database.php
+++ Questão 1/database.php
@@ -1,9 +1,7 @@
 <?php
-// Criação da conexão com o banco de dados SQLite
 function getDatabaseConnection() {
     $db = new SQLite3('livraria.db');
 
-    // Criação da tabela de livros caso ela não exista
     $db->exec("CREATE TABLE IF NOT EXISTS livros (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         titulo TEXT NOT NULL,
