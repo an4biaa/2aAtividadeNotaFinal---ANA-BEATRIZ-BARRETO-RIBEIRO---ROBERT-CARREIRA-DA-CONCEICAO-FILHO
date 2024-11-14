9f0cd8c0bf31ac65a8b9735d5085cca9125dd31d
Ana Beatriz Barreto Ribeiro <bibicachucle82@gmail.com>
2024-11-14 13:59:21 -0300

    delete_book.php

--- Questão 1/delete_book.php
+++ Questão 1/delete_book.php
@@ -1,19 +1,15 @@
 <?php
 include('database.php');
 
-// Verifica se o ID foi passado via GET
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
-    // Conexão com o banco de dados
     $db = getDatabaseConnection();
 
-    // Excluir o livro com o ID fornecido
     $stmt = $db->prepare('DELETE FROM livros WHERE id = :id');
     $stmt->bindValue(':id', $id, SQLITE3_INTEGER);
     $stmt->execute();
 
-    // Redirecionar para a página principal
     header('Location: index.php');
 }
 ?>
